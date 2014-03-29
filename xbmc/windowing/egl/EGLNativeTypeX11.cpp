8ee70b180e01f3ee7a5d2241da11b8a235e63578
Tuomas Kulve <tuomas@kulve.fi>
2014-03-29 17:42:09 +0000

    EGLNativeTypeX11: Use StringUtils.

--- xbmc/windowing/egl/EGLNativeTypeX11.cpp
+++ xbmc/windowing/egl/EGLNativeTypeX11.cpp
@@ -28,6 +28,7 @@
 #include "guilib/gui3d.h"
 #include "windowing/WinEvents.h"
 #include "windowing/WinEventsX11.h"
+#include "utils/StringUtils.h"
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
@@ -180,7 +181,7 @@ bool CEGLNativeTypeX11::GetNativeResolution(RESOLUTION_INFO *res) const
   res->fPixelRatio   = 1.0f;
   res->iScreenWidth  = res->iWidth;
   res->iScreenHeight = res->iHeight;
-  res->strMode.Format("%dx%d @ %.2f%s - Full Screen", res->iScreenWidth, res->iScreenHeight, res->fRefreshRate,
+  res->strMode = StringUtils::Format("%dx%d @ %.2f%s - Full Screen", res->iScreenWidth, res->iScreenHeight, res->fRefreshRate,
   res->dwFlags & D3DPRESENTFLAG_INTERLACED ? "i" : "");
   CLog::Log(LOGNOTICE,"Current resolution: %s\n",res->strMode.c_str());
   return true;
